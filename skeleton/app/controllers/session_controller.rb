3ab62015f6fd506dba493cff543b67e70c1302db
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-05-28 14:39:17 -0400

    session_controller new and create

--- skeleton/app/controllers/session_controller.rb
+++ skeleton/app/controllers/session_controller.rb
@@ -1,3 +1,20 @@
 class SessionController < ApplicationController
-  
+  def new 
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      #debugger
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login!(@user)
+      redirect_to users_url
+    else
+      render :new
+    end
+  end
 end
