a80b62e399f4df35766bf320ed52172415f3622d
Jon Chen <jonathan.chen.6799@gmail.com>
2021-05-28 13:21:15 -0500

    Fixed repo

---
+++ skeleton/app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ApplicationRecord
+  validates :session_token, :user_name, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(user_name: user_name)
+    return user if user && is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    password_object = BCrypt::Password.new(self.password_digest)
+    password_object.is_password?(password)
+  end
+
+  
+end
